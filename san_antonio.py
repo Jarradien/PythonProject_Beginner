afdfe489d770d1bb38dfdab6c5ef5f11f15fb434
Jarradien <41950604+Jarradien@users.noreply.github.com>
2019-12-20 22:23:42 +0100

    Update san_antonio.py

--- san_antonio.py
+++ san_antonio.py
@@ -7,16 +7,21 @@ quotes = [ "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas �
 
 characters = ["alvin et les Chipmunks","Babar","betty boop","calimero","casper","le chat potté", "Kirikou"]
 
-user_answer = input("Tapez une lettre")
-if user_answer == "B":
-	print("Ok je m'en vais... pfff")
-	exit()
-elif user_answer == "C":
-    print("C pas la bonne réponse ! Et G pas d’humour, je C...")
-
 
 def get_random_item_in(list):
 	item = list[1]
 	return item
 
 print(get_random_item_in(quotes))
+
+for i in range(len(characters)):
+    characters[i] = characters[i].capitalize()
+
+print(characters)
+
+# user_answer = input("Tapez une lettre")
+# if user_answer == "B":
+# 	print("Ok je m'en vais... pfff")
+# 	exit()
+# elif user_answer == "C":
+#     print("C pas la bonne réponse ! Et G pas d’humour, je C...")
